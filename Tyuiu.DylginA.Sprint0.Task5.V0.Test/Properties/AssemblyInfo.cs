32ca97de294622115fe3f5bb36052534112d158b
DylginAA <yababuff@yandex.ru>
2024-09-15 11:12:32 +0500

    Task5

---
+++ Tyuiu.DylginA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DylginA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DylginA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16ee635d-c210-43a3-9166-349a498de8c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DylginA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.DylginA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.DylginA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A+B=" +DataService.Addition(1,5));
+            Console.WriteLine("A-B=" + DataService.Addition(15, 5));
+            Console.WriteLine("A*B=" + DataService.Addition(10, 10));
+            Console.WriteLine("A/B=" + DataService.Addition(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
